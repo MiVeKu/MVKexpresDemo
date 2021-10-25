36df42e02f45e699df3e2fa21bf1c28f95c48d5f
Mikko Kuosmanen <mvkuosmanen@gmail.com>
2021-10-25 17:49:08 +0300

    fileRoutes.js started and TODO added.

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -2,4 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
+TODO add routes starting with post, get, delete
+use your own + from youtube https://www.youtube.com/watch?v=XCRUzPi0X0Q and https://www.youtube.com/watch?v=OvbRLY1QRIk
 */
